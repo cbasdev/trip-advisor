c4e011232a01f142d793b208c580f37b8298c851
sebasttianvelez <sebasttianvelez@gmail.com>
2020-08-10 12:01:15 -0500

    Proyect finalized

--- 5-tenedores/app/screens/TopRestaurants.js
+++ 5-tenedores/app/screens/TopRestaurants.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import { View, Text} from 'react-native'
+import React, { useState, useEffect, useRef } from "react"
+import { View } from "react-native"
+import Toast from "react-native-easy-toast"
+import ListTopRestaurants from "../components/Ranking/ListTopRestaurants"
 
-export default function TopRestaurants(){
-    return (
-        <View>
-            <Text>
-                Top Restaurants...
-            </Text>
-        </View>
-    )
+import { firebaseApp } from "../utils/firebase"
+import firebase from "firebase/app"
+import "firebase/firestore"
+
+const db = firebase.firestore(firebaseApp)
+
+export default function TopRestaurants(props) {
+  const { navigation } = props
+  const [restaurants, setRestaurants] = useState([])
+  const toastRef = useRef()
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .orderBy("rating", "desc")
+      .limit(5)
+      .get()
+      .then((response) => {
+        const restaurantArray = []
+        response.forEach((doc) => {
+          const data = doc.data()
+          data.id = doc.id
+          restaurantArray.push(data)
+        })
+        setRestaurants(restaurantArray)
+      })
+  }, [])
+
+  return (
+    <View>
+      <ListTopRestaurants restaurants={restaurants} navigation={navigation} />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+    </View>
+  )
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TripAdvisor
 
+*Realizada por: [Sebastian Velez Montoya](https://github.com/sebasttianvelez)*
+
+
+```bash
+git clone https://github.com/SebasttianVelez/tensor-flow-js
+```
+## Descripción y contexto
+
+React Native es un marco de aplicación móvil de código abierto creado por Facebook, Inc. Se utiliza para desarrollar aplicaciones para Android, iOS, Web y UWP al permitir a los desarrolladores usar React junto con las capacidades de la plataforma nativa. 
+
+[Descarga el APK de 5 Tenedores](https://expo.io/artifacts/95468a72-cb50-4f18-81fc-323405dec6a0)
+
+## Créditos
+
+Curso realizado por [Agustin Navarro Galdon](https://github.com/xAgustin93)
